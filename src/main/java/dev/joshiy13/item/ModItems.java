1bc101d5f78b8e363e2a600b495af634f4256ba2
Joshua <joshua@weidanz.de>
2024-10-07 09:09:50 +0200

    :(

--- src/main/java/dev/joshiy13/item/ModItems.java
+++ src/main/java/dev/joshiy13/item/ModItems.java
@@ -5,6 +5,6 @@ import dev.joshiy13.MoreFruits;
 
 public class ModItems {
     public static void registerModItems() {
-        MoreFruits.LOGGER.info("Registering Mod Items for " + MoreFruits.MOD_ID);
+        MoreFruits.LOGGER.info("Registering Items for " + MoreFruits.MOD_ID);
     }
 }
