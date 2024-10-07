a1e59a07bea86a9b7238c13bd45c1edb46b16931
Joshua <joshua@weidanz.de>
2024-10-07 13:09:50 +0200

    Added framework for adding items, added banana

--- src/main/java/dev/joshiy13/item/ModItems.java
+++ src/main/java/dev/joshiy13/item/ModItems.java
@@ -1,9 +1,19 @@
 package dev.joshiy13.item;
 
 import dev.joshiy13.MoreFruits;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 
 public class ModItems {
+    public static final Item BANANA = registerItem("banana", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MoreFruits.MOD_ID, name), item);
+    }
+
     public static void registerModItems() {
         MoreFruits.LOGGER.info("Registering Items for " + MoreFruits.MOD_ID);
     }
