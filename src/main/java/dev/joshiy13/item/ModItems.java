d990784be771a09dcb86c1eb4370e8117207141b
Joshua <joshua@weidanz.de>
2024-10-07 14:32:47 +0200

    Added Banana

--- src/main/java/dev/joshiy13/item/ModItems.java
+++ src/main/java/dev/joshiy13/item/ModItems.java
@@ -1,14 +1,20 @@
 package dev.joshiy13.item;
 
 import dev.joshiy13.MoreFruits;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 
 public class ModItems {
+
+    //Fruits
     public static final Item BANANA = registerItem("banana", new Item(new Item.Settings()));
+    //Berries
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MoreFruits.MOD_ID, name), item);
@@ -16,5 +22,12 @@ public class ModItems {
 
     public static void registerModItems() {
         MoreFruits.LOGGER.info("Registering Items for " + MoreFruits.MOD_ID);
+
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            //Fruits
+            entries.add(BANANA);
+        });
     }
 }
